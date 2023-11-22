033cbb851eea0f5b188d5b44055dc365b706836f
matecamilion <mateocamilion@gmail.com>
2023-11-22 11:44:28 -0300

    project changes

--- src/app/portfolio/proyectos/page.jsx
+++ src/app/portfolio/proyectos/page.jsx
@@ -1,21 +1,61 @@
-import Image from "next/image"
-export default function ProyectosPage() {    
+import Image from "next/image";
+import Link from "next/link";
 
-    return (
-        <section id="proyectos">
-             <div className=""> 
-                <h1 className=" flex justify-center text-3xl font-mono text-violet-800">PROYECTOS</h1> 
-                <hr  className="bg-violet-800 h-0.5  mx-10"/>
-            </div>
-            <div className="text-black text-center grid grid-cols-1 grid-rows-3 ">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-            </div>
-
-
-        </section>
-        
-    )
-    
-    }
+export default function ProyectosPage() {
+  return (
+    <section id="proyectos" className="container mx-auto px-4">
+      <div className="text-center">
+        <h1 className="text-3xl font-mono text-violet-800">PROYECTOS</h1>
+        <hr className="bg-violet-800 h-0.5 mx-10" />
+      </div>
+      <br />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <Link href="https://github.com/frantribu/shopix" className="flex flex-col items-center">
+          <div className="border border-violet-800 rounded overflow-hidden w-full max-w-md mb-4 md:mb-0">
+            <Image src="/shopix.png" width={400} height={400} objectFit="cover" className="w-full h-full" />
+          </div>
+          <div className="text-center text-black">
+            <h1 className="font-bold text-lg mt-4 md:mt-0">TIENDA SHOPIX</h1>
+            <p>
+              Este es un proyecto que realicé en la Diplomatura en Desarrollo Web con NextJS junto con Franco
+              Triboulard. El objetivo del proyecto era crear un e-commerce, es decir, una tienda online que permitiera
+              a los usuarios comprar y vender productos de diferentes categorías. Para ello, utilicé la fakestoreapi, una
+              API que proporciona datos falsos de productos, como el nombre, la descripción, el precio, la imagen y la
+              categoría.
+            </p>
+          </div>
+        </Link>
+        <Link href="https://github.com/frantribu/shopix" className="flex flex-col items-center">
+          <div className="border border-violet-800 rounded overflow-hidden w-full max-w-md mb-4 md:mb-0">
+            <Image src="/shopix.png" width={400} height={400} objectFit="cover" className="w-full h-full" />
+          </div>
+          <div className="text-center text-black">
+            <h1 className="font-bold text-lg mt-4 md:mt-0">TIENDA SHOPIX</h1>
+            <p>
+              Este es un proyecto que realicé en la Diplomatura en Desarrollo Web con NextJS junto con Franco
+              Triboulard. El objetivo del proyecto era crear un e-commerce, es decir, una tienda online que permitiera
+              a los usuarios comprar y vender productos de diferentes categorías. Para ello, utilicé la fakestoreapi, una
+              API que proporciona datos falsos de productos, como el nombre, la descripción, el precio, la imagen y la
+              categoría.
+            </p>
+          </div>
+        </Link>
+        <Link href="https://github.com/frantribu/shopix" className="flex flex-col items-center">
+          <div className="border border-violet-800 rounded overflow-hidden w-full max-w-md mb-4 md:mb-0">
+            <Image src="/shopix.png" width={400} height={400} objectFit="cover" className="w-full h-full" />
+          </div>
+          <div className="text-center text-black">
+            <h1 className="font-bold text-lg mt-4 md:mt-0">TIENDA SHOPIX</h1>
+            <p>
+              Este es un proyecto que realicé en la Diplomatura en Desarrollo Web con NextJS junto con Franco
+              Triboulard. El objetivo del proyecto era crear un e-commerce, es decir, una tienda online que permitiera
+              a los usuarios comprar y vender productos de diferentes categorías. Para ello, utilicé la fakestoreapi, una
+              API que proporciona datos falsos de productos, como el nombre, la descripción, el precio, la imagen y la
+              categoría.
+            </p>
+          </div>
+        </Link>
+      </div>
+    </section>
+  );
+}
--- src/app/portfolio/sobre-mi/page.jsx
+++ src/app/portfolio/sobre-mi/page.jsx
@@ -4,7 +4,7 @@ export default function SobreMiPage() {
 
   return (
     <section id="sobre-mi" className="p-8 sm:p-16 md:p-24 lg:p-32 xl:p-40">
-      <div id="about" className="flex flex-col items-center sm:flex-row justify-center  text-black space-y-8 sm:space-y-0 sm:space-x-10 h-full">
+      <div id="about" className="flex flex-col items-center sm:flex-row justify-center text-black space-y-8 sm:space-y-0 sm:space-x-10 h-full">
         <div className="w-full sm:w-2/3">
           <h1 className="text-3xl sm:text-5xl font-serif w-auto">
             Hola, mi nombre es{" "}
@@ -32,22 +32,23 @@ export default function SobreMiPage() {
             uso de este framework de React para crear aplicaciones web modernas y
             dinámicas.
           </p>
-          <Link href="https://www.canva.com/design/DAFNDSxkhCk/8_e6LWWmtrTxIi22QNoxlQ/view?utm_content=DAFNDSxkhCk&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel">
-          <button className="px-5 py-3 font-bold rounded-lg bg-gradient-to-r from-violet-500 to-fuchsia-500 text-black hover:shadow sm:w-44 md:w-48 lg:w-56 xl:w-64 ">Ver curriculum</button>
-        </Link>
+          <div className="flex justify-center mt-10">
+            <Link href="https://www.canva.com/design/DAFNDSxkhCk/8_e6LWWmtrTxIi22QNoxlQ/view?utm_content=DAFNDSxkhCk&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel">
+              <button className="px-5 py-3 font-bold rounded-lg bg-gradient-to-r from-violet-500 to-fuchsia-500 text-black hover:shadow sm:w-44 md:w-48 lg:w-56 xl:w-64">
+                Ver curriculum
+              </button>
+            </Link>
+          </div>
         </div>
 
-        <div className="mt-8 sm:mt-0 ">
+        <div className="mt-8 sm:mt-0">
           <img
-            className="rounded-full  w-32 sm:w-44 md:w-48 lg:w-56 xl:w-64 h-32 sm:h-44 md:h-48 lg:h-56 xl:h-64 object-cover"
+            className="rounded-full w-32 sm:w-44 md:w-48 lg:w-56 xl:w-64 h-32 sm:h-44 md:h-48 lg:h-56 xl:h-64 object-cover"
             src="https://media.licdn.com/dms/image/D4D03AQGjrD_0AZ1P9w/profile-displayphoto-shrink_800_800/0/1699475267203?e=1704931200&v=beta&t=5nxgV-yUU1V1ra_tfYw4xFulyHDQrtaFxfgQAn6oxtU"
             alt=""
           />
         </div>
-        
-       
       </div>
-    
     </section>
   )
 }
